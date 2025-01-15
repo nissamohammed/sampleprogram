8b91dcbdcf21d993542fb3ab3464106285630a28
nissamohammed <nissamohammed8@gmail.com>
2025-01-15 16:02:36 +0530

    some changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-//import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
-import Routing from './Routing.jsx'
-import {
+import App from './App.jsx'
+//import { BrowserRouter } from 'react-router-dom'
+//import Routing from './Routing.jsx'
+/*import {
   RecoilRoot,
   atom,
   selector,
   useRecoilState,
   useRecoilValue,
-} from 'recoil';
+} from 'recoil';*/
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RecoilRoot>
-      <BrowserRouter>
-        <Routing />
-
-      </BrowserRouter>
-    </RecoilRoot>
+        <App />
   </StrictMode>,
 )
